6fa364ee53b66813da38f81526cd4a7ef771bfd5
ardhikaxx <ardhikayanuar58@gmail.com>
2023-11-05 20:46:11 +0700

    first upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus, faTrash } from '@fortawesome/free-solid-svg-icons';
+
+interface Todo {
+  id: number;
+  text: string;
+  completed: boolean;
+}
 
 function App() {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [newTodo, setNewTodo] = useState<string>('');
+
+  const addTodo = () => {
+    if (newTodo.trim() !== '') {
+      const todo: Todo = {
+        id: new Date().getTime(),
+        text: newTodo,
+        completed: false,
+      };
+      setTodos([...todos, todo]);
+      setNewTodo('');
+    }
+  };
+
+  const toggleTodo = (id: number) => {
+    const updatedTodos = todos.map((todo) =>
+      todo.id === id ? { ...todo, completed: !todo.completed } : todo
+    );
+    setTodos(updatedTodos);
+  };
+
+  const deleteTodo = (id: number) => {
+    const updatedTodos = todos.filter((todo) => todo.id !== id);
+    setTodos(updatedTodos);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Todo List</h1>
+        <div className="todo-input">
+          <input
+            type="text"
+            placeholder="Tambahkan tugas baru"
+            value={newTodo}
+            onChange={(e) => setNewTodo(e.target.value)}
+          />
+          <button onClick={addTodo} className="add-button">
+            <FontAwesomeIcon icon={faPlus} /> Tambah
+          </button>
+        </div>
+        <ul className="todo-list">
+          {todos.map((todo) => (
+            <li key={todo.id} className={todo.completed ? 'completed' : ''}>
+              <span onClick={() => toggleTodo(todo.id)}>{todo.text}</span>
+              <button onClick={() => deleteTodo(todo.id)}>
+                <FontAwesomeIcon icon={faTrash} />
+              </button>
+            </li>
+          ))}
+        </ul>
       </header>
     </div>
   );
